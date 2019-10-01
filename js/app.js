e391f35e74df77a076682302289c666bae8c4026
Arthur PC <arthurpedroti@gmail.com>
2019-09-30 23:55:01 -0300

    balance-calc

--- js/app.js
+++ js/app.js
@@ -16,16 +16,43 @@ class UI {
     this.itemID = 0;
   }
   //submit budget method
-  submitBudgetForm(){
+  submitBudgetForm() {
     const value = this.budgetInput.value;
-    if( value === '' || value < 0) {
+    if ( value === '' || value < 0) {
       this.budgetFeedback.classList.add('showItem')
       this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
       const self = this;
       setTimeout(function(){
         self.budgetFeedback.classList.remove('showItem');
       },2000)
+    } else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
     }
+  }
+  //show balance
+  showBalance() {
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+    } 
+    else if (total > 0) {
+      this.balance.classList.remove('showRed', 'showBlack');
+      this.balance.classList.add('showGreen');
+    }
+    else if (total === 0) {
+      this.balance.classList.remove('showRed', 'showGreen');
+      this.balance.classList.add('showBlack');
+    }
+  }
+  //total expense
+  totalExpense() {
+    let total = 400;
+    return total;
   }
 }
 
