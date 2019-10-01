4d71349a2c3209bb092fbec50820c406fb858bc3
Arthur PC <arthurpedroti@gmail.com>
2019-09-30 23:31:08 -0300

    step1

--- js/app.js
+++ js/app.js
@@ -15,4 +15,42 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  //submit budget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if( value === '' || value < 0) {
+      this.budgetFeedback.classList.add('showItem')
+      this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+      const self = this;
+      setTimeout(function(){
+        self.budgetFeedback.classList.remove('showItem');
+      },2000)
+    }
+  }
 }
+
+function eventListenters() {
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  //new isntance of UI CLASS
+  const ui = new UI()
+
+  // budget form submit
+  budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  })
+  // expense form submit
+  expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+  })
+  // expense click
+  expenseList.addEventListener('click', function(){})
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  eventListenters();
+})
